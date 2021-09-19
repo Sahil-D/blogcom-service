c1d7e08dc4cfd95bab807a38b32110ed63bb25f4
Sahil-D <sunnydawara00@gmail.com>
2021-09-19 17:55:28 +0530

    Socket io Integration

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const socket = require('socket.io');
+
 const cors = require('cors');
-// const path = require('path');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
-const morganMiddleware = require('./middlewares/morgan');
-const ErrorMessages = require('./utils/ErrorMessages');
 const httpStatus = require('http-status');
+const morganMiddleware = require('./middlewares/morgan');
+
+const ErrorMessages = require('./utils/ErrorMessages');
 const ApiError = require('./utils/ApiError');
+
 const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
 const conversationRoute = require('./routes/conversation');
 const messageRoute = require('./routes/message');
+
 const PORT = process.env.PORT || 8080;
 
 dotenv.config();
@@ -28,7 +32,7 @@ app.use(helmet());
 app.use(morganMiddleware);
 
 app.get('/', (req, res) => {
-  res.send('Hello GET Request');
+  res.send('Welcome to BlogCom App Service');
 });
 
 app.use('/api/user', userRoute);
@@ -38,10 +42,57 @@ app.use('/api/message', messageRoute);
 
 // have to work on auth as middleware using JWT's
 
+// error handling part
 initErrorHandlers(app);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log('Server running at PORT : ', PORT);
+});
+
+// Socket Part
+const io = socket(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+let users = []; // List of all current users online { userId, socketId }
+
+const addUser = (userId, socketId) => {
+  users = users.filter((u) => u.userId !== userId);
+  users.push({ userId, socketId }); // result : we are getting in users[] -> online participants
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+// connecting event
+io.on('connection', (socket) => {
+  console.log('New User Logged In with socket ID ' + socket.id);
+  // take userId and socketId on every connection made because we can even have different socketId for same user also
+
+  socket.on('addUser', (userId) => {
+    addUser(userId, socket.id);
+    io.emit('getUsers', users); // returning list of online users to client side at getUser event
+  });
+
+  // send and receive messages
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    const user = users.find((u) => u.userId == receiverId); // opponent user
+
+    if (user)
+      io.to(user.socketId).emit('getMessage', {
+        senderId,
+        text,
+      });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A User disconnected with socket ID ' + socket.id);
+    removeUser(socket.id);
+    io.emit('getUsers', users); // returning list of modified online users to client side at getUser event
+  });
 });
 
 function initErrorHandlers(app) {
